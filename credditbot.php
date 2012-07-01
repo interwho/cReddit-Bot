8a0236d0c0881c83b533fd5b9c169ef8ffe47be2
Justin Paulin <Justin@Justin-Paulins-Macbook-Pro.local>
2012-07-01 19:59:12 -0400

    Upgraded To cRedditBot 2.0

--- credditbot.php
+++ credditbot.php
@@ -24,6 +24,34 @@ function curlGet($url)	{
 	return $return;
 }
 
+//Date Converter
+function time_ago($date,$granularity=2) {
+    $difference = time() - $date;
+    $periods = array(
+        'year' => 31536000,
+        'month' => 2628000,
+        'week' => 604800, 
+        'day' => 86400,
+        'hour' => 3600,
+        'minute' => 60,
+        'second' => 1);
+
+    foreach ($periods as $key => $value) {
+        if ($difference >= $value) {
+            $time = floor($difference/$value);
+            $difference %= $value;
+            $retval .= ($retval ? ' ' : '').$time.' ';
+            $retval .= (($time > 1) ? $key.'s' : $key);
+            $granularity--;
+        }
+        if ($granularity == '0') { break; }
+    }
+    if(strlen($retval) == 0) {
+    	$retval = 'an instant';
+    }
+    return 'joined: '.$retval.' ago';      
+}
+
 //User Rater
 function rateUser($post,$id) {
 	//Get Username By Parsing Post Description
@@ -68,15 +96,22 @@ function rateUser($post,$id) {
 	$hash = $loginvars->{'modhash'};
 	$cookie = $loginvars->{'cookie'};
 	$cookie = urlencode($cookie);
+	$idl = $id;
 	$id = 't3_'.$id;
-	$message = "**The following is your cReddit rating:**\n\n
-[Account Created: **$acctage**](/meta_)\n\n
-[Total Karma: **$karma**](/meta_)\n\n
-[Total # of Loans Requested: **$req**](/req_)\n\n
-[Total # of Loans Granted To Other Redditors by This Redditor: **$granted**](/offer_)\n\n
-[Total # of Loans Paid Back by/to This Redditor: **$paid**](/paid_)\n\n
-[Total # of Loans NOT Paid Back by/to This Redditor: **$unpaid**](/unpaid_)\n\n
-\n\n**[I am an automated bot. If you have any questions or feedback, please contact my owner, interwhos.](http://www.reddit.com/u/interwhos)**";
+	$acctage = time_ago($acctage);
+	$message = "Stats for **[$username](http://www.reddit.com/r/Random_Acts_Of_Pizza/search?q=author%3A%27$username%27&restrict_sr=on)** on RAoP\n\n
+---------------------------------------\n\n
+* [$req Loans Requested](/req_)
+* [$granted Loans Granted To Others](/offer_)
+* [$paid Loans Paid Back By/To This Redditor](/paid_)
+* [$unpaid Loans NOT Paid Back By/To This Redditor](/unpaid_)\n\n
+---------------------------------------\n\n
+[$acctage - total karma: $karma](/meta_)\n\n
+---------------------------------------\n\n
+[report link](http://www.reddit.com/message/compose?to=%2Fr%2FLoans&subject=cRedditBot%20Link%20Reported%20-%20".urlencode('http://redd.it/'.$idl).") or [send feedback](http://www.reddit.com/message/compose?to=interwhos&subject=cRedditBot%20Feedback!)\n\n
+---------------------------------------\n\n
+Hi! I'm cRedditBot 2.0.\n\n
+---------------------------------------";
 	$urltopost = "http://www.reddit.com/api/comment";
 	$datatopost = array(
 		"thing_id" => $id,
