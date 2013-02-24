40d350e952aaee5ad45ba72105bf6dbcd8944e3a
Justin Paulin <Justin@MacBook-Pro.local>
2013-02-23 21:01:11 -0500

    Updated to 2.1
    Separated Lender/Requester Stats

--- credditbot.php
+++ credditbot.php
@@ -30,7 +30,7 @@ function time_ago($date,$granularity=2) {
     $periods = array(
         'year' => 31536000,
         'month' => 2628000,
-        'week' => 604800, 
+        'week' => 604800,
         'day' => 86400,
         'hour' => 3600,
         'minute' => 60,
@@ -49,7 +49,7 @@ function time_ago($date,$granularity=2) {
     if(strlen($retval) == 0) {
     	$retval = 'an instant';
     }
-    return 'joined: '.$retval.' ago';      
+    return 'joined: '.$retval.' ago';
 }
 
 //User Rater
@@ -58,7 +58,7 @@ function rateUser($post,$id) {
 	preg_match('#http://www.reddit.com/user/(.*?)">#', $post->description, $username);
 	$username = $username[0];
 	$username = preg_replace('#http://www.reddit.com/user/#', '', $username);
-	$username = preg_replace('#">#', '', $username);	
+	$username = preg_replace('#">#', '', $username);
 	//Get Username Info
 	$response = curlGet("http://www.reddit.com/user/$username/about.json");
 	$response = json_decode($response);
@@ -69,14 +69,18 @@ function rateUser($post,$id) {
 	$acctage = time_ago($acctage);
 	//Search Username And Get Variables
 	$response = curlGet("http://www.reddit.com/r/Loans/search.xml?syntax=cloudsearch&q=author%3A%27$username%27&restrict_sr=on&sort=new");
-	$req = substr_count($response, '<title>[REQ]');	
-	$granted = substr_count($response, '<title>[PAID]');
-	$granted = $granted + substr_count($response, '<title>[UNPAID]');
+	$response = strtoupper($response);
+	$req = substr_count($response, '<TITLE>[REQ]');
+	$req = $req + substr_count($response, '<TITLE>[REQUEST]');
+	$granted = substr_count($response, '<TITLE>[PAID]');
+	$gpaid = substr_count($response, '<TITLE>[PAID]');
+	$granted = $granted + substr_count($response, '<TITLE>[UNPAID]');
+	$gunpaid = substr_count($response, '<TITLE>[UNPAID]');
 	//Search For Paid & Unpaid Loans
-	$response = curlGet("http://www.reddit.com/r/Loans/search.xml?syntax=cloudsearch&q=$username+%5BPAID%5D&restrict_sr=on&sort=new");
-	$paid = substr_count($response, '<title>[PAID]');
-	$response = curlGet("http://www.reddit.com/r/Loans/search.xml?syntax=cloudsearch&q=$username+%5BUNPAID%5D&restrict_sr=on&sort=new");
-	$unpaid = substr_count($response, '<title>[UNPAID]');
+	$response = strtoupper(curlGet("http://www.reddit.com/r/Loans/search.xml?syntax=cloudsearch&q=title:$username+%5BPAID%5D&restrict_sr=on&sort=new"));
+	$paid = substr_count($response, '<TITLE>[PAID]');
+	$response = strtoupper(curlGet("http://www.reddit.com/r/Loans/search.xml?syntax=cloudsearch&q=title:$username+%5BUNPAID%5D&restrict_sr=on&sort=new"));
+	$unpaid = substr_count($response, '<TITLE>[UNPAID]');
 	//Add The Comment
 	$urltopost = "https://ssl.reddit.com/api/login/cRedditBot";
 	$datatopost = array (
@@ -98,18 +102,28 @@ function rateUser($post,$id) {
 	$cookie = urlencode($cookie);
 	$idl = $id;
 	$id = 't3_'.$id;
-	$message = "Stats for **[$username](http://www.reddit.com/r/Random_Acts_Of_Pizza/search?q=author%3A%27$username%27&restrict_sr=on)** on r/Loans\n\n
+	//Temp - This breaks things with the new separated stats though...
+	//$paid = $paid - $gpaid;
+	//$unpaid = $unpaid - $gunpaid;
+	//End temp
+	$message = "Stats for **[$username](http://www.reddit.com/r/Loans/search?q=author%3A%27$username%27&restrict_sr=on)** on r/Loans\n\n
 ---------------------------------------\n\n
-* [$req Loans Requested](/req_)
-* [$granted Loans Granted To Others](/offer_)
-* [$paid Loans Paid Back By/To This Redditor](/paid_)
-* [$unpaid Loans NOT Paid Back By/To This Redditor](/unpaid_)\n\n
+**Requester Stats**\n\n
+* [$req Loan(s) Requested](/req_)
+* [$paid Loan(s) Paid Back By This Redditor](/paid_)
+* [$unpaid Loan(s) NOT Paid Back By This Redditor](/unpaid_)\n\n
+**Lender Stats**\n\n
+* [$granted Loan(s) Granted To Others](/offer_)
+* [$gpaid Loan(s) Paid Back To This Redditor](/paid_)
+* [$gunpaid Loan(s) NOT Paid Back To This Redditor](/unpaid_)\n\n
 ---------------------------------------\n\n
 [$acctage - total karma: $karma](/meta_)\n\n
 ---------------------------------------\n\n
 [report link](http://www.reddit.com/message/compose?to=%2Fr%2FLoans&subject=cRedditBot%20Link%20Reported%20-%20".urlencode('http://redd.it/'.$idl).") or [send feedback](http://www.reddit.com/message/compose?to=interwhos&subject=cRedditBot%20Feedback!)\n\n
 ---------------------------------------\n\n
-[Hi! I'm cRedditBot 2.0.](/meta_)\n\n
+[Important Info For Lenders Regarding Scams](http://www.reddit.com/r/Loans/comments/14imky/meta_double_loans_to_the_same_scammer/)\n\n
+---------------------------------------\n\n
+[Hi! I'm the cRedditBot. I'm here to help protect the lenders that make this place great. Please keep in mind that I AM a bot, so you won't get a response if you PM me.](/meta_)\n\n
 ---------------------------------------";
 	$urltopost = "http://www.reddit.com/api/comment";
 	$datatopost = array(
@@ -133,7 +147,7 @@ function rateUser($post,$id) {
 
 //Start Script Logic
 $loansbase = curlGet("http://www.reddit.com/r/Loans/new.xml?sort=new");
-$loansbase = preg_replace('#<title>Loans(.*?)</image>#', '//', $loansbase);
+$loansbase = preg_replace('#<TITLE>Loans(.*?)</image>#', '//', $loansbase);
 $loansbase = simplexml_load_string($loansbase);
 $loansbase = $loansbase->channel;
 
